80a847b9e95b94e37bb54cff97c46c306b528e30
datisekai <datly030102@gmail.com>
2023-03-21 23:22:15 +0700

    add progressbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,24 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import 'react-lazy-load-image-component/src/effects/blur.css';
+import "react-lazy-load-image-component/src/effects/blur.css";
+import NextNProgress from "nextjs-progressbar";
 
 import "~/styles/globals.css";
-import {  QueryClientProvider, QueryClient } from 'react-query'
+import { QueryClientProvider, QueryClient } from "react-query";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
 
   return (
     <SessionProvider session={session}>
-       <QueryClientProvider client={queryClient}>
-
-      <Component {...pageProps} />
-       </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <NextNProgress color="#cc66ff"/>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </SessionProvider>
   );
 };
